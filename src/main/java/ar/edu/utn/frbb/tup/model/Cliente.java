b4c228390ec7c7e6b67a433b307ee93616772baf
GenaroWeis <142353631+GenaroWeis@users.noreply.github.com>
2024-12-11 23:05:03 -0300

    first commit

--- src/main/java/ar/edu/utn/frbb/tup/model/Cliente.java
+++ src/main/java/ar/edu/utn/frbb/tup/model/Cliente.java
@@ -1,6 +1,9 @@
 package ar.edu.utn.frbb.tup.model;
 
-import ar.edu.utn.frbb.tup.controller.ClienteDto;
+import ar.edu.utn.frbb.tup.controller.dto.ClienteDto;
+import ar.edu.utn.frbb.tup.model.enumModels.TipoCuenta;
+import ar.edu.utn.frbb.tup.model.enumModels.TipoMoneda;
+import ar.edu.utn.frbb.tup.model.enumModels.TipoPersona;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -8,21 +11,27 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Cliente extends Persona{
 
-    private TipoPersona tipoPersona;
-    private String banco;
-    private LocalDate fechaAlta;
-    private Set<Cuenta> cuentas = new HashSet<>();
+public class Cliente extends Persona {
+
+    
+    private TipoPersona tipoPersona; 
+    private String banco; 
+    private LocalDate fechaAlta; 
+    private Set<Cuenta> cuentas = new HashSet<>(); // Colección de cuentas asociadas al cliente
+    private Set<Prestamo> prestamos = new HashSet<>(); // Colección de préstamos asociados al cliente
 
     public Cliente() {
         super();
     }
+    // Constructor: inicializa un Cliente a partir del DTO
     public Cliente(ClienteDto clienteDto) {
         super(clienteDto.getDni(), clienteDto.getApellido(), clienteDto.getNombre(), clienteDto.getFechaNacimiento());
-        fechaAlta = LocalDate.now();
-        banco = clienteDto.getBanco();
+        tipoPersona = TipoPersona.fromString(clienteDto.getTipoPersona());// tipo de persona a string
+        fechaAlta = LocalDate.now(); 
+        banco = clienteDto.getBanco(); 
     }
+
 
     public TipoPersona getTipoPersona() {
         return tipoPersona;
@@ -52,21 +61,44 @@ public class Cliente extends Persona{
         return cuentas;
     }
 
-    public void addCuenta(Cuenta cuenta) {
-        this.cuentas.add(cuenta);
-        cuenta.setTitular(this);
+    public Set<Prestamo> getPrestamos() {
+        return prestamos;
     }
 
+    // AGREGAR UNA CUENTA AL CLIENTE
+    public void addCuenta(Cuenta cuenta) {
+        this.cuentas.add(cuenta); // Añade la cuenta a la lista de cuentas
+        cuenta.setTitular(cuenta.getTitular()); // Actualiza el titular de la cuenta
+    }
+
+    // VERIFICA SI EL CLIENTE TIENE UNA CUENTA (en base al tipo y moneda)
     public boolean tieneCuenta(TipoCuenta tipoCuenta, TipoMoneda moneda) {
-        for (Cuenta cuenta:
-                cuentas) {
-            if (tipoCuenta.equals(cuenta.getTipoCuenta()) && moneda.equals(cuenta.getMoneda())) {
+        for (Cuenta cuenta : cuentas) {
+            if (tipoCuenta.equals(cuenta.getTipoCuenta()) && moneda.equals(cuenta.getMoneda())) {// Si si true
                 return true;
             }
         }
-        return false;
+        return false; // si no false
     }
 
+    // VERIFICA SI EL CLIENTE TIENE UNA CUENTA CON UNA MONEDA ESPECIFICA
+    public boolean tieneCuentaMoneda(TipoMoneda moneda) {
+        for (Cuenta cuenta : cuentas) {
+            if (moneda.equals(cuenta.getMoneda())) {// Si si true
+                return true;
+            }
+        }
+        return false; // si no false
+    }
+
+
+    // AGREGAR UN PRESTAMO AL CLIENTE
+    public void addPrestamo(Prestamo prestamo) {
+        this.prestamos.add(prestamo); // Añade el préstamo a la lista de préstamos.
+        prestamo.setNumeroCliente(prestamo.getNumeroCliente()); // Actualiza el número de cliente en el préstamo.
+    }
+
+    // REPRESENTACION EN TEXTO DEL CLIENTE
     @Override
     public String toString() {
         return "Cliente{" +
